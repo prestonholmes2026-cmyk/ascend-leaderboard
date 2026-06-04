6181d956a62dad0119fcf76afb37cbd63028ec33
prestonholmes2026-cmyk <prestonholmes2026@gmail.com>
2026-06-04 14:39:11 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './Home.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/leaderboard" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
+
